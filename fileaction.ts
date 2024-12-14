54e9b5b8cf480ba57c1befdbee8329c00b6cd6de
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-12-14 07:17:48 +0300

    Create fileaction.ts

---
+++ fileaction.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import {import all the files that you want to be accessed};
+
+ const handleError =(error: uknown, message: string) =>{
+  console.log(error, message);
+  throw error;
+ };
+
+export const uploadFile = async({
+  file,
+  ownerId,
+  accountId,
+  path,
+}: UploadFileProps) =>{
+  const {storage, database} = await createAdminClient();
+
+  try{
+    const imputFile = InputFile.fromBuffer(file, file.name);
+     const bucketFile = await storage.createFile(
+       appwriteConfig.bucketId,
+       ID.unique(),
+       inputFile,
+     );
+    const 
+  }
+}
